c151aee8440f6ef73ee9db53e22460960676a27a
LeoCatto96 <lcatto@ar0fvfgw3p6q05n.ml.com>
2022-07-28 17:51:50 -0300

    Refresh the repo

---
+++ express-clase/ejercitacion/middleware/admin.js
@@ -0,0 +1,39 @@
+/**
+En este archivo vamos a implementar diferentes funcionalidades
+en este caso, usamos "user" para crear un objeto con información de usuarios
+y "auth" para mostrar un mensaje determinado. Éstas funciones middleware
+las utilizamos en "/routes/products.js"
+
+que es res.locals? : https://www.geeksforgeeks.org/express-js-res-locals-property/
+*/
+/**
+ * Esta funcion deberia guardar en res.locals.user un objeto con atributos id,name,admin recibidos por body 
+ */
+
+function user(req, res, next) {
+
+	res.locals.user = {
+		name: 'pepe',
+		id: 2,
+		admin: true
+	}
+
+		next()
+	
+};
+
+
+ function auth(req, res,next) {
+
+	const { name, admin } = res.locals.user;
+
+	if(admin){
+		res.send(`El usuario ${name}, es admin`);
+	}else{
+		res.send(`Error Auth`);
+	}
+	
+	next()
+};
+
+module.exports = {user,auth}
